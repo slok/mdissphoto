a932953eed9053af8c3670473006e2bbc80a3573
Xabier Larrakoetxea <slok69@gmail.com>
2012-02-22 22:51:58 +0100

    Added mongo-utils project structure

---
+++ mongo-utils/src/main/java/org/mdissjava/mongoutils/App.java
@@ -0,0 +1,13 @@
+package org.mdissjava.mongoutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
