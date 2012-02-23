e8959be8ceda1a2ec03c8947e54bbeebbe8e026b
Xabier Larrakoetxea <slok69@gmail.com>
2012-02-23 19:32:39 +0100

    Added mdiss-core web application

---
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/App.java
@@ -0,0 +1,13 @@
+package org.mdissjava.mdisscore.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
