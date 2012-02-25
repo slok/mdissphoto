3b5672663eb18f3a545717977fcb867ddbdc62f5
Xabier Larrakoetxea <slok69@gmail.com>
2012-02-25 16:51:01 +0100

    Added logging (of log) to the GridFS tests

--- common-utils/src/test/java/org/mdissjava/commonutils/mongo/gridfs/GridfsDataStorerTest.java
+++ common-utils/src/test/java/org/mdissjava/commonutils/mongo/gridfs/GridfsDataStorerTest.java
@@ -11,14 +11,19 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 public class GridfsDataStorerTest {
 
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Test
 	public void saveDataTest() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] saveDataTest()");
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		//load the file to store
 		InputStream is = getClass().getResourceAsStream("/slackware.png");
@@ -28,6 +33,7 @@ public class GridfsDataStorerTest {
 	@Test(expected=IOException.class)
 	public void saveDataTestWrongConnection() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] saveDataTestWrongConnection");
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test","127.0.0.2", 80 );
 		//load the file to store
 		InputStream is = getClass().getResourceAsStream("/slackware.png");
@@ -37,6 +43,8 @@ public class GridfsDataStorerTest {
 	@Test(expected=IllegalStateException.class)
 	public void saveDataTestNull() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] saveDataTestNull");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		gds.saveData(null);	
 	}
@@ -44,6 +52,8 @@ public class GridfsDataStorerTest {
 	@Test
 	public void saveDataTestWithCustomName() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] saveDataTestWithCustomName");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		//load the file to store
 		InputStream is = getClass().getResourceAsStream("/slackware.png");
@@ -53,6 +63,8 @@ public class GridfsDataStorerTest {
 	@Test
 	public void retrieveData() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] retrieveData");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		OutputStream baos = gds.getData("myCustomName");
 		
@@ -62,6 +74,8 @@ public class GridfsDataStorerTest {
 	@Test(expected=IOException.class)
 	public void retrieveMissingData() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] retrieveMissingData");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		gds.getData("D03sn'7 3x15t");
 	}
@@ -69,6 +83,8 @@ public class GridfsDataStorerTest {
 	@Test
 	public void sameInSameOut() throws IOException, NoSuchAlgorithmException
 	{
+		this.logger.info("[TEST][GridFS] sameInSameOut");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 
 		InputStream is = getClass().getResourceAsStream("/slackware.png");
@@ -89,6 +105,8 @@ public class GridfsDataStorerTest {
 	@Test
 	public void notSameInSameOut() throws IOException, NoSuchAlgorithmException
 	{
+		this.logger.info("[TEST][GridFS] notSameInSameOut");
+		
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 
 		InputStream is = getClass().getResourceAsStream("/slackware.png");
@@ -108,6 +126,8 @@ public class GridfsDataStorerTest {
 	@Test(expected=IOException.class)
 	public void deleteData() throws IOException
 	{
+		this.logger.info("[TEST][GridFS] deleteData");
+		
 		String id = "7h15 w1ll n07 574y much 71m3 1n d474b453";
 		GridfsDataStorer gds = new GridfsDataStorer("mdissphoto", "test");
 		//load the file to store
