576bc92a5baab101030c4f8e6d44602649972763
David <David@David-PC>
2012-03-04 15:58:20 +0100

    Metadata and Photo POJOS created

---
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/pojo/Metadata.java
@@ -0,0 +1,238 @@
+package org.mdissjava.mdisscore.model.pojo;
+
+import java.util.Date;
+
+import com.google.code.morphia.annotations.Embedded;
+import com.google.code.morphia.annotations.Reference;
+
+/**
+ * Represents all of the metadata of a single photo.
+ * 
+ * @author mdiss_java
+ */
+@Embedded
+public class Metadata {
+
+	/** The camera which took the photo */
+	@Reference
+	private Camera camera;
+
+	/** The shutter speed in seconds. */
+	private String shutterSpeed;
+
+	/** The aperture. */
+	private String aperture;
+
+	/** The focal length in millimeters. */
+	private Integer focalLength;
+
+	/** The ISO speed. */
+	private Integer ISOSpeed;
+
+	/** The sensor size. */
+	private Integer sensorSize;
+
+	/** The resolution. */
+	@Embedded
+	private Resolution resolution;
+
+	/** The size in KiloBytes. */
+	private Float size;
+
+	/** The date taken. */
+	private Date dateTaken;
+
+	/** The format of the file (Ex. JPG, BMP, PNG...). */
+	private String format;
+
+	/**
+	 * Gets the camera.
+	 * 
+	 * @return the camera
+	 */
+	public Camera getCamera() {
+		return this.camera;
+	}
+
+	/**
+	 * Sets the camera.
+	 * 
+	 * @param camera
+	 *            the new camera
+	 */
+	public void setCamera(Camera camera) {
+		this.camera = camera;
+	}
+
+	/**
+	 * Gets the shutter speed.
+	 * 
+	 * @return the shutter speed
+	 */
+	public String getShutterSpeed() {
+		return this.shutterSpeed;
+	}
+
+	/**
+	 * Sets the shutter speed.
+	 * 
+	 * @param shutterSpeed
+	 *            the new shutter speed
+	 */
+	public void setShutterSpeed(String shutterSpeed) {
+		this.shutterSpeed = shutterSpeed;
+	}
+
+	/**
+	 * Gets the aperture.
+	 * 
+	 * @return the aperture
+	 */
+	public String getAperture() {
+		return this.aperture;
+	}
+
+	/**
+	 * Sets the aperture.
+	 * 
+	 * @param aperture
+	 *            the new aperture
+	 */
+	public void setAperture(String aperture) {
+		this.aperture = aperture;
+	}
+
+	/**
+	 * Gets the focal length.
+	 * 
+	 * @return the focal length
+	 */
+	public Integer getFocalLength() {
+		return this.focalLength;
+	}
+
+	/**
+	 * Sets the focal length.
+	 * 
+	 * @param focalLength
+	 *            the new focal length
+	 */
+	public void setFocalLength(Integer focalLength) {
+		this.focalLength = focalLength;
+	}
+
+	/**
+	 * Gets the iSO speed.
+	 * 
+	 * @return the iSO speed
+	 */
+	public Integer getISOSpeed() {
+		return this.ISOSpeed;
+	}
+
+	/**
+	 * Sets the iSO speed.
+	 * 
+	 * @param iSOSpeed
+	 *            the new iSO speed
+	 */
+	public void setISOSpeed(Integer iSOSpeed) {
+		this.ISOSpeed = iSOSpeed;
+	}
+
+	/**
+	 * Gets the sensor size.
+	 * 
+	 * @return the sensor size
+	 */
+	public Integer getSensorSize() {
+		return this.sensorSize;
+	}
+
+	/**
+	 * Sets the sensor size.
+	 * 
+	 * @param sensorSize
+	 *            the new sensor size
+	 */
+	public void setSensorSize(Integer sensorSize) {
+		this.sensorSize = sensorSize;
+	}
+
+	/**
+	 * Gets the resolution.
+	 * 
+	 * @return the resolution
+	 */
+	public Resolution getResolution() {
+		return this.resolution;
+	}
+
+	/**
+	 * Sets the resolution.
+	 * 
+	 * @param resolution
+	 *            the new resolution
+	 */
+	public void setResolution(Resolution resolution) {
+		this.resolution = resolution;
+	}
+
+	/**
+	 * Gets the size.
+	 * 
+	 * @return the size
+	 */
+	public Float getSize() {
+		return this.size;
+	}
+
+	/**
+	 * Sets the size.
+	 * 
+	 * @param size
+	 *            the new size
+	 */
+	public void setSize(Float size) {
+		this.size = size;
+	}
+
+	/**
+	 * Gets the date taken.
+	 * 
+	 * @return the date taken
+	 */
+	public Date getDateTaken() {
+		return this.dateTaken;
+	}
+
+	/**
+	 * Sets the date taken.
+	 * 
+	 * @param dateTaken
+	 *            the new date taken
+	 */
+	public void setDateTaken(Date dateTaken) {
+		this.dateTaken = dateTaken;
+	}
+
+	/**
+	 * Gets the format.
+	 * 
+	 * @return the format
+	 */
+	public String getFormat() {
+		return this.format;
+	}
+
+	/**
+	 * Sets the format.
+	 * 
+	 * @param format
+	 *            the new format
+	 */
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+}
---
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/pojo/Photo.java
@@ -0,0 +1,280 @@
+package org.mdissjava.mdisscore.model.pojo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+import com.google.code.morphia.annotations.Embedded;
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Reference;
+
+/**
+ * Represents a photo took by a user.
+ * 
+ * @author mdiss_java
+ */
+@Entity
+public class Photo {
+
+	/** The unique id. */
+	@Id
+	private ObjectId id;
+
+	/** The title of the photo. */
+	private String title;
+
+	/** Represents if the photo is public, can be seen by anyone, or private. */
+	private Boolean publicPhoto;
+
+	/** The list of all voted the photo has received. */
+	@Embedded
+	private List<Vote> votes;
+
+	/** The date when the photo was uploaded. */
+	private Date uploadDate;
+
+	/**
+	 * The next photo in the sequence, used to short the photos in the users
+	 * gallery.
+	 */
+	@Reference
+	private Photo nextPhoto;
+
+	/**
+	 * The previous photo in the sequence, used to short the photos in the users
+	 * gallery.
+	 */
+	@Reference
+	private Photo backwardPhoto;
+
+	/** The metadata. */
+	@Embedded
+	private Metadata metadata;
+
+	/** The list of tags associated to the photo . */
+	private List<String> tags;
+
+	/** The id of the photo's data in the database. */
+	private String dataId;
+
+	/** Represents if the photo is only for adults or not. */
+	private Boolean plus18;
+
+	public Photo() {
+		tags = new ArrayList<String>();
+		votes = new ArrayList<Vote>();
+	}
+
+	/**
+	 * Gets the id.
+	 * 
+	 * @return the id
+	 */
+	public ObjectId getId() {
+		return this.id;
+	}
+
+	/**
+	 * Sets the id.
+	 * 
+	 * @param id
+	 *            the new id
+	 */
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the title.
+	 * 
+	 * @return the title
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+
+	/**
+	 * Sets the title.
+	 * 
+	 * @param title
+	 *            the new title
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * Gets the public photo.
+	 * 
+	 * @return the public photo
+	 */
+	public Boolean getPublicPhoto() {
+		return this.publicPhoto;
+	}
+
+	/**
+	 * Sets the public photo.
+	 * 
+	 * @param publicPhoto
+	 *            the new public photo
+	 */
+	public void setPublicPhoto(Boolean publicPhoto) {
+		this.publicPhoto = publicPhoto;
+	}
+
+	/**
+	 * Gets the vote.
+	 * 
+	 * @return the vote
+	 */
+	public List<Vote> getVotes() {
+		return this.votes;
+	}
+
+	/**
+	 * Sets the vote.
+	 * 
+	 * @param votes
+	 *            the new vote
+	 */
+	public void setVote(List<Vote> votes) {
+		this.votes = votes;
+	}
+
+	/**
+	 * Gets the upload date.
+	 * 
+	 * @return the upload date
+	 */
+	public Date getUploadDate() {
+		return this.uploadDate;
+	}
+
+	/**
+	 * Sets the upload date.
+	 * 
+	 * @param uploadDate
+	 *            the new upload date
+	 */
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	/**
+	 * Gets the next photo.
+	 * 
+	 * @return the next photo
+	 */
+	public Photo getNextPhoto() {
+		return this.nextPhoto;
+	}
+
+	/**
+	 * Sets the next photo.
+	 * 
+	 * @param nextPhoto
+	 *            the new next photo
+	 */
+	public void setNextPhoto(Photo nextPhoto) {
+		this.nextPhoto = nextPhoto;
+	}
+
+	/**
+	 * Gets the backward photo.
+	 * 
+	 * @return the backward photo
+	 */
+	public Photo getBackwardPhoto() {
+		return this.backwardPhoto;
+	}
+
+	/**
+	 * Sets the backward photo.
+	 * 
+	 * @param backwardPhoto
+	 *            the new backward photo
+	 */
+	public void setBackwardPhoto(Photo backwardPhoto) {
+		this.backwardPhoto = backwardPhoto;
+	}
+
+	/**
+	 * Gets the metadata.
+	 * 
+	 * @return the metadata
+	 */
+	public Metadata getMetadata() {
+		return this.metadata;
+	}
+
+	/**
+	 * Sets the metadata.
+	 * 
+	 * @param metadata
+	 *            the new metadata
+	 */
+	public void setMetadata(Metadata metadata) {
+		this.metadata = metadata;
+	}
+
+	/**
+	 * Gets the tags.
+	 * 
+	 * @return the tags
+	 */
+	public List<String> getTags() {
+		return this.tags;
+	}
+
+	/**
+	 * Sets the tags.
+	 * 
+	 * @param tags
+	 *            the new tags
+	 */
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	/**
+	 * Gets the data id.
+	 * 
+	 * @return the data id
+	 */
+	public String getDataId() {
+		return this.dataId;
+	}
+
+	/**
+	 * Sets the data id.
+	 * 
+	 * @param dataId
+	 *            the new data id
+	 */
+	public void setDataId(String dataId) {
+		this.dataId = dataId;
+	}
+
+	/**
+	 * Gets the plus18.
+	 * 
+	 * @return the plus18
+	 */
+	public Boolean getPlus18() {
+		return this.plus18;
+	}
+
+	/**
+	 * Sets the plus18.
+	 * 
+	 * @param plus18
+	 *            the new plus18
+	 */
+	public void setPlus18(Boolean plus18) {
+		this.plus18 = plus18;
+	}
+
+}
