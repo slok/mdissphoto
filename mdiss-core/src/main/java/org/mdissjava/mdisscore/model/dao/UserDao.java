ff3a3cefa272461036784690f1c84fffd80296ce
Oihane Kamara <oihanek@gmail.com>
2012-03-08 09:58:44 +0100

    UserDaoImpl: Method addUser now operates through Hibernate instead of Morphia. Deleted the implementations of the other methods as well.

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/UserDao.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/UserDao.java
@@ -7,13 +7,17 @@ import org.mdissjava.mdisscore.model.pojo.Configuration;
 import org.mdissjava.mdisscore.model.pojo.User;
 
 
-
 public interface UserDao {
 
-	String addUser(User user);
+	void addUser(User user); 
+	
 	void deleteUser(User user);
+	
 	void loggedIn(ObjectId id);
+	
 	User getUserById(ObjectId id);
+	
 	void modifyAddress(ObjectId id, Address address);
+	
 	void modifyConfiguration(ObjectId id, Configuration conf);
 }
--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/impl/UserDaoImpl.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/impl/UserDaoImpl.java
@@ -3,80 +3,57 @@ package org.mdissjava.mdisscore.model.dao.impl;
 import java.util.Date;
 
 import org.bson.types.ObjectId;
-import org.mdissjava.mdisscore.model.dao.LoginDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.mdissjava.mdisscore.model.dao.UserDao;
+import org.mdissjava.mdisscore.model.dao.hibernate.HibernateUtil;
 import org.mdissjava.mdisscore.model.pojo.Address;
 import org.mdissjava.mdisscore.model.pojo.Configuration;
-import org.mdissjava.mdisscore.model.pojo.Login;
 import org.mdissjava.mdisscore.model.pojo.User;
 
-import com.google.code.morphia.Datastore;
-import com.google.code.morphia.dao.BasicDAO;
-import com.google.code.morphia.mapping.Mapper;
-import com.google.code.morphia.query.Query;
-import com.google.code.morphia.query.UpdateOperations;
 
+public class UserDaoImpl implements UserDao {
+	
+	public UserDaoImpl() {
 
-public class UserDaoImpl extends BasicDAO<User, ObjectId> implements UserDao {
-
-	LoginDao loginDao;
-	
-	public UserDaoImpl(Datastore ds) {
-		super(ds);
-		loginDao = new LoginDaoImpl();
-	}
-	
-	
-	private Query<User> queryToFindMe(ObjectId id){
-		   return ds.createQuery(User.class).field(Mapper.ID_KEY).equal(id);
-	}
+	}	
 	
 	@Override
-	public String addUser(User user) {
-		ds.save(user);
-		Login login = new Login(user.getId().toString(), user.getEmail(), user.getPass());
-		loginDao.addLoginDetails(login);
-		return user.getId().toString();
-		
+	public void addUser(User user) {
+		if(user != null){
+			Session session = HibernateUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			session.save(user);
+			tx.commit();
+		}
 	}
 
 	@Override
 	public void deleteUser(User user) {
-		ds.delete(user);	
+
 	}
 		
 	@Override
 	public User getUserById( ObjectId id ) {	  
-	    return ds.get(User.class, id);
+		return null;
 	}
 	
 	
 	@Override
 	public void loggedIn(ObjectId id){
 	   Date now = new Date();
-	   UpdateOperations<User> ops = ds.createUpdateOperations(User.class).set("lastSession", now);
-	   ds.update(queryToFindMe(id), ops);
+	   
 	
 	}
 		
 	@Override
 	public void modifyAddress(ObjectId id, Address address){
-		   UpdateOperations<User> ops = ds.createUpdateOperations(User.class).set("street", address.getStreet()).
-				   						set("city", address.getCity()).set("country", address.getCountry()).
-				   						set("state", address.getState()).set("zip",address.getZip());
-		   ds.update(queryToFindMe(id), ops);
+
 	}
 	
 	@Override
 	public void modifyConfiguration(ObjectId id, Configuration conf){
-		   UpdateOperations<User> ops = ds.createUpdateOperations(User.class).set("showNick", conf.isShowNick()).
-				   						set("showName", conf.isShowName()).set("showSurname", conf.isShowSurname()).
-				   						set("showEmail", conf.isShowEmail()).set("showBirthdate",conf.isShowBirthdate()).
-				   						set("showPhone", conf.isShowPhone()).set("showGender", conf.isShowGender()).
-				   						set("showCity", conf.isShowCity()).set("showCountry",conf.isShowCountry()).
-				   						set("showAddress", conf.isShowAddress()).set("showZip",conf.isShowZip()).
-				   						set("showCameras", conf.isShowCameras());
-		   ds.update(queryToFindMe(id), ops);
+
 	}
 
 }
