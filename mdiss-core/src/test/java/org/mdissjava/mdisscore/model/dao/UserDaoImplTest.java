1decebd6838b27ff5c4f807f1131ec653e33b4a3
Oihane Kamara <oihanek@gmail.com>
2012-03-08 10:12:17 +0100

    Test now operated through Hibernate instead of Morphia. Removed addLoginTest.

--- mdiss-core/src/test/java/org/mdissjava/mdisscore/model/dao/UserDaoImplTest.java
+++ mdiss-core/src/test/java/org/mdissjava/mdisscore/model/dao/UserDaoImplTest.java
@@ -4,56 +4,34 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
 
+import org.hibernate.Session;
+import org.junit.Before;
 import org.junit.Test;
-import org.mdissjava.mdisscore.model.dao.factory.MorphiaDatastoreFactory;
 import org.mdissjava.mdisscore.model.dao.hibernate.HibernateUtil;
 import org.mdissjava.mdisscore.model.dao.impl.UserDaoImpl;
 import org.mdissjava.mdisscore.model.pojo.Address;
 import org.mdissjava.mdisscore.model.pojo.Configuration;
 import org.mdissjava.mdisscore.model.pojo.User;
 import org.mdissjava.mdisscore.model.pojo.User.Gender;
-
-import com.google.code.morphia.Datastore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class UserDaoImplTest {
 
-
+	private Session session ;
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	
-	public void addUserTest(){
-		Datastore ds = MorphiaDatastoreFactory.getDatastore("test");
-		Address address = new Address();		
-		address.setStreet("Madariaga 6");		
-		address.setCity("Bilbao");
-		address.setState("Vizcaya");
-		address.setCountry("Spain");
-		
-		Configuration conf = new Configuration();
-										
-		User user = new User();
-		user.setNick("jess");
-		user.setName("Jessica");		
-		user.setSurname("Smith");
-		user.setPhone(944655877);
-		user.setBirthdate(new Date());
-		user.setGender(Gender.Female);
-		user.setAddress(address);
-		user.setConfiguration(conf);		
-		user.addPreference("nature");
-		user.addPreference("horses");
-		user.addPreference("sunsets");
-		
-		UserDao dao = new UserDaoImpl(ds);
-		dao.addUser(user);		
-		assertEquals(user, ds.get(User.class, user.getId()));		
+	@Before
+	public void setUp() throws Exception {
+		HibernateUtil.openSessionFactory();
+		session = HibernateUtil.getSession();
 	}
-	
 
 	@Test
-	public void addLogin(){		
-		Datastore ds = MorphiaDatastoreFactory.getDatastore("test");
-		
+	public void addUserTest(){		
+		this.logger.info("TEST(UserDao) addUser");
+	
 		Address address = new Address();		
 		address.setStreet("Madariaga 6");		
 		address.setCity("Bilbao");
@@ -77,12 +55,9 @@ public class UserDaoImplTest {
 		user.setEmail("prueba@prueba.com");
 		user.setPass("prueba");
 		
-		HibernateUtil.openSessionFactory();
-		UserDao dao = new UserDaoImpl(ds);
-		dao.addUser(user);		
-		
-		//assertEquals(user, ds.get(User.class, user.getId()));		
-		
+		UserDao dao = new UserDaoImpl();
+		dao.addUser(user);				
+		assertEquals(user, session.get(User.class, user.getId()));				
 	}
 
 }
