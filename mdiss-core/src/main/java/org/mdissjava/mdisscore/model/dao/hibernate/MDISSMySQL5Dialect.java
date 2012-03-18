64754b342f1ae4a2d46fbabd80edb6d14fadcb47
Xabier Larrakoetxea <slok69@gmail.com>
2012-03-18 17:30:50 +0100

    Indentation minor changes and comments in MDISSMySQL5Dialect

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/hibernate/MDISSMySQL5Dialect.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/hibernate/MDISSMySQL5Dialect.java
@@ -4,9 +4,18 @@ import java.sql.Types;
 
 import org.hibernate.dialect.MySQL5Dialect;
 
+/**
+ * There is a problem with the new hibernate way of getting the boolean values.
+ * To fix this problem the normal MySQL5Dialect is extended adn chage the 
+ * way Hibernate maps the boolean values from MySQL
+ * 
+ * @author MDISS Java team 2011-2012 University of Deusto
+ *
+ */
 public class MDISSMySQL5Dialect extends MySQL5Dialect{ 
-public MDISSMySQL5Dialect() {
-super();
-registerColumnType( Types.BOOLEAN, "bit(1)" ); 
-} 
+
+	public MDISSMySQL5Dialect() {
+		super();
+		registerColumnType( Types.BOOLEAN, "bit(1)" ); 
+	} 
 }
