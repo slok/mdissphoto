6294ddd506ce8b38b8b88aa658445b95d2aa848d
Xabier Larrakoetxea <slok69@gmail.com>
2012-03-28 22:00:46 +0200

    Added some a wrong connection test to Gearman workers

--- thumbnailer/src/test/java/org/mdissjava/thumbnailer/gearman/GearmanWorkersTest.java
+++ thumbnailer/src/test/java/org/mdissjava/thumbnailer/gearman/GearmanWorkersTest.java
@@ -1,6 +1,7 @@
 package org.mdissjava.thumbnailer.gearman;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.ConnectException;
@@ -13,19 +14,24 @@ import org.gearman.worker.GearmanFunction;
 import org.junit.Test;
 import org.mdissjava.thumbnailer.gearman.worker.ThumbnailerGearmanWorker;
 import org.mdissjava.thumbnailer.gearman.worker.ThumbnailerScaleFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GearmanWorkersTest {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Test
 	public void testConnetion() throws InvalidAttributeValueException, InterruptedException, ConnectException {
 
+		this.logger.info("Starting testConnection test");
+		
 		//add functions
 		List<Class<GearmanFunction>> functions = new ArrayList<Class<GearmanFunction>>();
 		Class gf = ThumbnailerScaleFunction.class;
 		functions.add((Class<GearmanFunction>)gf);
 		
-		//TODO: Exception
 		ThumbnailerGearmanWorker worker = new ThumbnailerGearmanWorker(functions);
 		
 		//start worker (creating a thread brecause we want to finish the test!)
@@ -37,6 +43,31 @@ public class GearmanWorkersTest {
 		//Dary!
 		workerThread.stop();
 		assertFalse(workerThread.isException());
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void testWrongConnetion() throws InvalidAttributeValueException, InterruptedException, ConnectException {
+
+		this.logger.info("Starting testWrongConnection test");
+		
+		//add functions
+		List<Class<GearmanFunction>> functions = new ArrayList<Class<GearmanFunction>>();
+		Class gf = ThumbnailerScaleFunction.class;
+		functions.add((Class<GearmanFunction>)gf);
+		
+		ThumbnailerGearmanWorker worker = new ThumbnailerGearmanWorker("localhost", 9876,functions);
+		
+		//start worker (creating a thread brecause we want to finish the test!)
+		workerThread workerThread =  new workerThread(worker);
+		//Legen...
+		new Thread (workerThread).start();
+		//Wait for it...
+		Thread.sleep(500); //for the connection
+		//Dary!
+		workerThread.stop();
+		assertTrue(workerThread.isException());
+		
 	}
 
 	//helper class for threading...
