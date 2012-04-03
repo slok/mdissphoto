467dbc183668e83bc4e9338d286964c7260fb7c0
Inigo Elejalde <inigorst21@gmail.com>
2012-04-03 18:32:11 +0200

    delete login bll

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/bll/LoginBll.java
+++
@@ -1,8 +0,0 @@
-package org.mdissjava.mdisscore.model.bll;
-
-import org.mdissjava.mdisscore.model.bo.UserBo;
-
-public interface LoginBll {
-	UserBo Login(String email,String password);
-	boolean Logout(UserBo user);
-}
