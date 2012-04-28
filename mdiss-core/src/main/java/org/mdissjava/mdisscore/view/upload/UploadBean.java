0419c208dde6b0ed0ae728f861d12052be770876
Xabier Larrakoetxea <slok69@gmail.com>
2012-04-28 10:27:56 +0200

    Removed hardcoded things from upload

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/view/upload/UploadBean.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/view/upload/UploadBean.java
@@ -1,5 +1,6 @@
 package org.mdissjava.mdisscore.view.upload;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,12 +12,16 @@ import javax.faces.context.FacesContext;
 import org.mdissjava.commonutils.mongo.gridfs.GridfsDataStorer;
 import org.mdissjava.commonutils.photo.status.PhotoStatusManager;
 import org.mdissjava.commonutils.properties.PropertiesFacade;
+import org.mdissjava.mdisscore.controller.bll.impl.PhotoManagerImpl;
+import org.mdissjava.mdisscore.metadata.impl.MetadataExtractorImpl;
 import org.mdissjava.mdisscore.model.dao.factory.MorphiaDatastoreFactory;
+import org.mdissjava.mdisscore.model.pojo.Metadata;
 import org.mdissjava.mdisscore.view.params.ParamsBean;
 import org.primefaces.event.FileUploadEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.drew.metadata.MetadataException;
 import com.google.code.morphia.Datastore;
 
 @RequestScoped
@@ -32,18 +37,22 @@ public class UploadBean {
 	private String imageDB;
 	private String originalImageBucket;
 	
+	private final String GLOBALS_PROPS_KEY = "globals";
+	private final String MORPHIA_DATABASE_KEY = "morphia.db";
+	private final String IMAGES_DATABASE_KEY = "images.db";
+	private final String ORIGINAL_IMG_BUCKET_KEY = "images.bucket";
+	
 	public UploadBean()
 	{
 		try {
 			ParamsBean pb = getPrettyfacesParams();
 			this.userId = pb.getUserId();
-			//TODO: Load from properties
 			
 			Properties globals;
-			globals = new PropertiesFacade().getProperties("globals");
-			morphiaDB = globals.getProperty("morphia.db");
-			imageDB = globals.getProperty("images.db");
-			originalImageBucket  = globals.getProperty("images.bucket");
+			globals = new PropertiesFacade().getProperties(GLOBALS_PROPS_KEY);
+			morphiaDB = globals.getProperty(MORPHIA_DATABASE_KEY);
+			imageDB = globals.getProperty(IMAGES_DATABASE_KEY);
+			originalImageBucket  = globals.getProperty(ORIGINAL_IMG_BUCKET_KEY);
 	
 			gfs = new GridfsDataStorer(imageDB, originalImageBucket);
 			
@@ -81,15 +90,32 @@ public class UploadBean {
 		try{
 			//Save in database
 			this.logger.info("Entering the upload");
-			InputStream data =  event.getFile().getInputstream();
-			this.imageId = gfs.saveData(data);
+			InputStream originalData =  event.getFile().getInputstream();
+			
+			this.imageId = gfs.saveData(originalData);
 			this.logger.info("upload and stored: {}", this.imageId);
 			
-			//set the new status to the photo
+			//the inputStream is empty we need to retrieve again
+			ByteArrayOutputStream data = (ByteArrayOutputStream)gfs.getData(this.imageId);
+			
+			//get the datastore
 			Datastore ds = MorphiaDatastoreFactory.getDatastore(this.morphiaDB);
+			//get metadata and save in database
+			try
+			{
+				System.out.println(data.toByteArray());
+				Metadata metadata = new MetadataExtractorImpl().obtenerMetadata(data.toByteArray());	
+				new PhotoManagerImpl(ds).insertMetadata(imageId, metadata);
+				
+			}catch (MetadataException e) {
+				e.printStackTrace();
+				this.logger.error("Error extracting metadata");
+			}
+			System.out.println("hor8");
+			//set the new status to the photo
 			PhotoStatusManager photoManager = new PhotoStatusManager(ds);
 			photoManager.createPhotoStatus(this.imageId);
-			
+			System.out.println("horl9");
 			//set the paramas in the url
 			ParamsBean params = getPrettyfacesParams();
 			params.setPhotoId(this.imageId);
@@ -98,7 +124,7 @@ public class UploadBean {
 			outcome = "pretty:user_upload_processing";
 			
 		}catch(Exception e){
-			
+			System.out.println(e);
 			outcome = "pretty:user_upload_error";
 		
 		}finally{
