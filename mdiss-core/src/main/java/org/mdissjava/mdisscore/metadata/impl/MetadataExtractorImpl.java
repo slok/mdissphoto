a3a4cb605066590d67a053de69a0acbccc3fce42
Maitefru <maitefru@gmail.com>
2012-04-30 16:19:35 +0200

    Improvement in Metadata Exif and Basic extraction, now supporting GIF, TIFF and PNG. Added RealResolution field to metadata.

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/metadata/impl/MetadataExtractorImpl.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/metadata/impl/MetadataExtractorImpl.java
@@ -1,17 +1,13 @@
 package org.mdissjava.mdisscore.metadata.impl;
 
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.mdissjava.mdisscore.metadata.MetadataExtractor;
+import org.mdissjava.mdisscore.metadata.SimpleImageInfo;
 import org.mdissjava.mdisscore.model.pojo.Metadata;
-import org.mdissjava.mdisscore.model.pojo.Photo;
 import org.mdissjava.mdisscore.model.pojo.Resolution;
 
 import com.drew.imaging.ImageMetadataReader;
@@ -19,59 +15,114 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.MetadataException;
 import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
-import com.google.code.morphia.Datastore;
-import com.google.code.morphia.dao.BasicDAO;
-import com.google.code.morphia.mapping.Mapper;
-import com.google.code.morphia.query.Query;
-import com.google.code.morphia.query.UpdateOperations;
-
-import eu.medsea.mimeutil.MimeUtil;
 
 public class MetadataExtractorImpl implements MetadataExtractor {
+	
+	private byte[] photo = null;
+	private SimpleImageInfo simpleImage = null;
 	
 	@Override
 	public Metadata obtenerMetadata(byte[] photo) throws MetadataException, ImageProcessingException, IOException {
 		
-		MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
-        Collection<?> mimeTypes = MimeUtil.getMimeTypes(photo);
-        String format = getOnlyExtension(mimeTypes.toString());
-		
+		this.photo = photo;
+		this.simpleImage = new SimpleImageInfo(photo);
+        String format = getOnlyExtension(simpleImage.getMimeType());
+        
 		if((format.equals("jpeg"))||(format.equals("tiff")))
 		{
-			Metadata metadata = new Metadata();
-			Resolution resolution = new Resolution();
-						
-			ByteArrayInputStream photoInputStream = new ByteArrayInputStream(photo);
-			BufferedInputStream bufferedPhoto = new BufferedInputStream(photoInputStream);
+			return getEXIFMetadata(format);
+			
+		}
+		else if((format.equals("png"))||(format.equals("gif")))
+		{
+			return getBasicMetadata(format);
+			
+		}
+		else
+		{
+			System.out.println("EXIF metadata not supported for the format: "+format);
+			return getBasicMetadata(format);
+		}
+		
+			
+	}
+	
+	@Override
+	public Metadata getEXIFMetadata(String format) {
+
+		Metadata metadata = new Metadata();
+		Resolution resolution = new Resolution();
+					
+		ByteArrayInputStream photoInputStream = new ByteArrayInputStream(photo);
+		BufferedInputStream bufferedPhoto = new BufferedInputStream(photoInputStream);
+		
+		try{
+		
 			
 			com.drew.metadata.Metadata metadataFoto = ImageMetadataReader.readMetadata(bufferedPhoto, true);
-		       
+			
+			Resolution resolutionREAL = new Resolution();
+			resolutionREAL.setHeight(this.simpleImage.getHeight());
+			resolutionREAL.setWidth(this.simpleImage.getWidth());	
+			
+			System.out.println("X "+resolutionREAL.getWidth()+" Y "+resolutionREAL.getHeight());
+
 			ExifIFD0Directory exifIFD0Directory = metadataFoto.getDirectory(ExifIFD0Directory.class);
 			ExifSubIFDDirectory exifSubIFDirectory = metadataFoto.getDirectory(ExifSubIFDDirectory.class);
-			
+					
 			metadata.setShutterSpeed(exifSubIFDirectory.getString(ExifSubIFDDirectory.TAG_SHUTTER_SPEED));
 			metadata.setAperture(exifSubIFDirectory.getString(ExifSubIFDDirectory.TAG_APERTURE));
 			metadata.setFocalLength(exifSubIFDirectory.getInt(ExifSubIFDDirectory.TAG_FOCAL_LENGTH));
 			metadata.setISOSpeed(exifSubIFDirectory.getInt(ExifSubIFDDirectory.TAG_ISO_EQUIVALENT));
 			resolution.setHeight(exifIFD0Directory.getInt(ExifIFD0Directory.TAG_Y_RESOLUTION));
 			resolution.setWidth(exifIFD0Directory.getInt(ExifIFD0Directory.TAG_X_RESOLUTION));
-			metadata.setResolution(resolution);
+			metadata.setResolutionPPI(resolution);
+			metadata.setResolutionREAL(resolution);
 			metadata.setDateTaken(exifIFD0Directory.getDate(exifIFD0Directory.TAG_DATETIME));
 			metadata.setFormat(format);
 			metadata.setSize((float)bytesToMb(photo.length));
 			
 			return metadata;
-			
 		}
-		else
+		catch(NullPointerException npe)
 		{
-			System.out.println("Metadata not supported for the format: "+format);
+			//npe.printStackTrace();
 			return null;
 		}
+		catch(ImageProcessingException ipe){
+			//ipe.printStackTrace();
+			return null;
+		}	
+		catch(IOException io){
+			//io.getStackTrace();
+			return null;
+		}
+	    catch(MetadataException me)
+	    {
+	    	System.out.println("No metadata available");
+	    	me.getStackTrace();
+	    	return null;
+	    }
 		
-			
 	}
 
+
+	@Override
+	public Metadata getBasicMetadata(String format) throws MetadataException,
+			ImageProcessingException, IOException {
+		
+		Metadata metadata = new Metadata();
+		Resolution resolution = new Resolution();
+		
+		resolution.setWidth(this.simpleImage.getWidth());
+		resolution.setHeight(this.simpleImage.getHeight());
+		
+		metadata.setResolutionREAL(resolution);
+		metadata.setFormat(format);
+		metadata.setSize((float)bytesToMb(photo.length));
+		
+		return metadata;
+	}
 
 	@Override
 	public String getOnlyExtension(String type) {
@@ -95,5 +146,6 @@ public class MetadataExtractorImpl implements MetadataExtractor {
 	 public long bytesToMb(int bytes) {
 	  return bytes / MEGABYTE ;
 	 }
+
 }
 
