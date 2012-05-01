4e80cbef0780bc0d40ee345c0a5f560bcd198451
Inigo Elejalde <inigorst21@gmail.com>
2012-05-01 19:26:07 +0200

    global variable to min user years

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/view/registration/BirthdateValidator.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/view/registration/BirthdateValidator.java
@@ -1,6 +1,8 @@
 package org.mdissjava.mdisscore.view.registration;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Properties;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -9,23 +11,42 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+import org.mdissjava.commonutils.properties.PropertiesFacade;
+
 
 @FacesValidator("org.mdissjava.mdisscore.view.registration.BirthdateValidator")
 public class BirthdateValidator implements Validator{
 
+	
+	private final String GLOBAL_PROPS_KEY = "globals";
+	private final String MIN_USER_AGE = "min_user_age.var";
+	
+	
 	@Override
 	public void validate(FacesContext context, UIComponent component,
 			Object value) throws ValidatorException {
 
 		Date fecha=(Date)value;
-
+		
+		String edad="18";
+		try {
+		 edad= new PropertiesFacade().getProperties(GLOBAL_PROPS_KEY).getProperty(MIN_USER_AGE);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		Date aux=new Date();
-		aux.setYear(aux.getYear()-18);
+		
+			aux.setYear(aux.getYear()- Integer.parseInt(edad));
+		
+		
 		if(fecha.compareTo(aux)>0)
 		{
 			FacesMessage msg = 
-					new FacesMessage("you must be 18 years old", 
-							"you must be 18 years old");
+					new FacesMessage("you must be "+edad+" years old", 
+							"you must be "+edad+" years old");
 				msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 				throw new ValidatorException(msg);
 		}
