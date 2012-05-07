160fa0a4b3786a6ba1fa653f149c754b1813b10a
Oihane Kamara <oihanek@gmail.com>
2012-05-07 09:48:06 +0200

    Changed the notation of methods and modified getFollows function in accordance to the new Hibernate notations

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/UserDao.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/UserDao.java
@@ -11,29 +11,34 @@ public interface UserDao {
 
 	void addUser(User user); 
 	
+	void updateUser(User user);
+	
 	void deleteUser(User user);
 	
 	void loggedIn(ObjectId id);
 	
 	User getUserById(int id);
 	
-	boolean emailAllReadyExists(String email);
-	
-	boolean nickAllReadyExists(String nick);
-	
 	User getUserByNick(String username);
+	
+	boolean emailAlreadyExists(String email);
+	
+	boolean nickAlreadyExists(String nick);
 	
 //	void modifyAddress(ObjectId id, Address address);
 	
 //	void modifyConfiguration(ObjectId id, Configuration conf);
 	
-	void updateUser(User user);
+	List<User> findFollows(User user);
 	
-	List<User> findFriends(User user);
+	List<User> findFollowers(User user);
 	
-	void addFriend(int userid, int friendid);
+	void addFollow(int userid, int friendid);
 	
-	void deleteFriend(int userid, int friendid);
+	void deleteFollow(int userid, int friendid);
 	
-//	Configuration findConfiguration(User user);
+	void addFollower(int userid, int friendid);
+	
+	void deleteFollower(int userid, int friendid);	
+
 }
--- mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/impl/UserDaoImpl.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/model/dao/impl/UserDaoImpl.java
@@ -23,32 +23,6 @@ public class UserDaoImpl implements UserDao {
 	}	
 	
 	@Override
-	public boolean emailAllReadyExists(String email)
-	{
-		Session session = HibernateUtil.getSession();
-
-		int num = (Integer) session.createQuery("from User where email = '"+email+"'").list().size();
-	//	session.close();
-		if(num>0)
-			return true;
-		else
-			return false;
-	}
-	
-	@Override
-	public boolean nickAllReadyExists(String nick)
-	{
-		Session session = HibernateUtil.getSession();
-
-		int num = (Integer) session.createQuery("from User where nick = '"+nick+"'").list().size();
-	//	session.close();
-		if(num>0)
-			return true;
-		else
-			return false;
-	}
-	
-	@Override
 	public void addUser(User user) {
 		if(user != null){
 			System.out.println("DAo // addUser*************");
@@ -60,40 +34,6 @@ public class UserDaoImpl implements UserDao {
 		}
 	}
 
-	@Override
-	public void deleteUser(User user) {
-		if (user != null) {
-			Session session = HibernateUtil.getSession();
-			Transaction tx = session.beginTransaction();
-			session.delete(user);
-			tx.commit();
-		//	session.close();
-		}
-	}
-		
-	@Override
-	public User getUserById( int id ) {	  
-
-		User user = null;
-		Session session = HibernateUtil.getSession();
-
-		Query q = session.createQuery("" + "from User as user "
-				+ "where user.id =" + id);
-		user = (User) q.uniqueResult();
-	//	session.close();
-		
-		return user;
-	
-	}
-	
-	
-	@Override
-	public void loggedIn(ObjectId id){
-	   Date now = new Date();
-	   
-	
-	}
-		
 
 	@Override
 	public void updateUser(User user) {
@@ -106,43 +46,119 @@ public class UserDaoImpl implements UserDao {
 		//	session.close();
 		}		
 	}
-
-	@SuppressWarnings("unchecked")
+	
 	@Override
-	public List<User> findFriends(User user) {
-		List<User> users = new ArrayList<User>();
+	public void deleteUser(User user) {
+		if (user != null) {
+			Session session = HibernateUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			session.delete(user);
+			tx.commit();
+		//	session.close();
+		}
+	}
+	
+	@Override
+	public boolean emailAlreadyExists(String email) {
 		Session session = HibernateUtil.getSession();
 
-		Query q = session.createQuery("" + "from friends as users "
-				+ "where friends.userId =" + user.getId());
-		users =  q.list();
+		int num = (Integer) session.createQuery("from User where email = '"+email+"'").list().size();
 	//	session.close();
-		return users;
+		if(num>0)
+			return true;
+		else
+			return false;
 	}
-
+	
 	@Override
-	public void addFriend(int userid, int friendid) {
+	public boolean nickAlreadyExists(String nick) {
+		Session session = HibernateUtil.getSession();
 
-		
+		int num = (Integer) session.createQuery("from User where nick = '"+nick+"'").list().size();
+	//	session.close();
+		if(num>0)
+			return true;
+		else
+			return false;
 	}
-
-	@Override
-	public void deleteFriend(int userid, int friendid) {
-		// TODO Auto-generated method stub
+	
 		
-	}
-
 	@Override
-	public User getUserByNick(String nick) {
-		
+	public User getUserById( int id ) {	  
 		User user = null;
 		Session session = HibernateUtil.getSession();
+		Query q = session.createQuery("" + "from User as user "
+				+ "where user.id =" + id);
+		user = (User) q.uniqueResult();
+	//	session.close();		
+		return user;	
+	}
+	
+	@Override
+	public User getUserByNick(String nick) {		
+		User user = null;
+	Session session = HibernateUtil.getSession();
 		
 		Query q = session.createQuery("" + "from User as user where user.nick =" + "'" + nick + "'");
 		user = (User) q.uniqueResult();
 	//	session.close();
 	//	System.out.println("getUserByNick: insert "+nick +" con Nick recuperado: "+user.getNick());
 		return user;
+	}
+	
+	
+	@Override
+	public void loggedIn(ObjectId id){
+	   Date now = new Date();
+	  	
+	}
+		
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> findFollows(User user) {
+		List<User> users = new ArrayList<User>();
+		Session session = HibernateUtil.getSession();
+
+		Query q = session.createQuery("Select follows from User as user "
+				+ " where user.id =" + user.getId());
+								
+		users =  q.list();
+		//	session.close();
+		
+		// user.getFollows();	
+		return users;
+	}
+
+	@Override
+	public void addFollow(int userid, int friendid) {
+		//TODO: 
+	}
+
+	@Override
+	public void deleteFollow(int userid, int friendid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public List<User> findFollowers(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addFollower(int userid, int friendid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteFollower(int userid, int friendid) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
