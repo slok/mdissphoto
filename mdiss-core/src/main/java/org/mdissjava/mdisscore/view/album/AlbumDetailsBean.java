e27579f7abc2ff98313ded6f765f23df7c44858d
Iraide Diaz <iraide.diaz@gmail.com>
2012-05-19 19:15:55 +0200

    Started album detail page.

---
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/view/album/AlbumDetailsBean.java
@@ -0,0 +1,33 @@
+package org.mdissjava.mdisscore.view.album;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.mdissjava.mdisscore.model.pojo.Album;
+import org.mdissjava.mdisscore.model.pojo.Photo;
+import org.mdissjava.mdisscore.view.params.ParamsBean;
+
+@RequestScoped
+@ManagedBean
+public class AlbumDetailsBean {
+	
+	public AlbumDetailsBean()
+	{	
+		
+	}
+
+	private ParamsBean getPrettyfacesParams()
+	{
+		FacesContext context = FacesContext.getCurrentInstance();
+		ParamsBean pb = (ParamsBean) context.getApplication().evaluateExpressionGet(context, "#{paramsBean}", ParamsBean.class);
+		return pb;
+	}
+
+}
