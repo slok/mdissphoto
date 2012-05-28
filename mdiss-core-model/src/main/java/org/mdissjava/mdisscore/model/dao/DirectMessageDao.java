28a8c7a7565ba927950b4c372e7d566bd69cc938
Jose Maeso <jose.maesog@gmail.com>
2012-05-28 15:34:48 +0200

    Dao changed for more usefull method and Impl completed

--- mdiss-core-model/src/main/java/org/mdissjava/mdisscore/model/dao/DirectMessageDao.java
+++ mdiss-core-model/src/main/java/org/mdissjava/mdisscore/model/dao/DirectMessageDao.java
@@ -9,11 +9,11 @@ public interface DirectMessageDao {
 	
 	void insertDirectMessage(DirectMessage directMessage);
 	
-	void updateDirectMessage(DirectMessage directMessage);
+	void markAsRead(String messageId);
 	
 	void deleteDirectMessage(DirectMessage directMessage);
 	
-	List<DirectMessage> findDirectMessage(DirectMessage directMessage);
+	List<DirectMessage> findDirectMessage(DirectMessage directMessage, boolean readAndNotRead);
 	
 	List<DirectMessage> findDirectMessageBetween(Date from, Date to);
 
