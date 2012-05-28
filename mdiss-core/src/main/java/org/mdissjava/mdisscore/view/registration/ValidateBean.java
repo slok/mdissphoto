031b1823874bcdde214f934db21c97aa18ad6fe2
Jose Maeso <jose.maesog@gmail.com>
2012-05-28 12:05:01 +0200

    Added xhtml and methods in Bean for key resend

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/view/registration/ValidateBean.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/view/registration/ValidateBean.java
@@ -1,16 +1,24 @@
 package org.mdissjava.mdisscore.view.registration;
 
+import java.util.logging.Logger;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.validator.ValidatorException;
 
 import org.mdissjava.mdisscore.model.dao.KeyLinkDao;
 import org.mdissjava.mdisscore.model.dao.UserDao;
 import org.mdissjava.mdisscore.model.dao.factory.MorphiaDatastoreFactory;
 import org.mdissjava.mdisscore.model.dao.impl.KeyLinkDaoImpl;
 import org.mdissjava.mdisscore.model.dao.impl.UserDaoImpl;
+import org.mdissjava.mdisscore.model.pojo.KeyLink;
+import org.mdissjava.mdisscore.model.pojo.User;
 import org.mdissjava.mdisscore.view.params.ParamsBean;
+import org.mdissjava.notifier.event.manager.NotificationManager;
+import org.mdissjava.notifier.event.observable.VerifyAccountObservable;
 
 import com.google.code.morphia.Datastore;
 
@@ -19,8 +27,12 @@ import com.google.code.morphia.Datastore;
 public class ValidateBean {
 
 	private String key;
-	private UserDao userDao=new UserDaoImpl();
+	private UserDao userDao = new UserDaoImpl();
 	private boolean valid;
+	private String email;
+
+	private static Logger logger = Logger.getLogger(RegistrationBean.class
+			.getName());
 
 	@PostConstruct
 	public void init() {
@@ -41,6 +53,35 @@ public class ValidateBean {
 			// TODO: Activate User using the userId
 			userDao.activateUser(userId);
 		}
+	}
+
+	public void resend(ActionEvent actionEvent) throws ValidatorException {
+
+		ValidateBean.logger.info("Validation Email resend");
+
+		User user = userDao.getUserByEmail(this.email);
+		if (user != null && !user.isActive()) {
+			valid = true;
+			Datastore db = MorphiaDatastoreFactory.getDatastore("test");
+			KeyLinkDao keyLinkDao = new KeyLinkDaoImpl(db);
+			KeyLink keylink = new KeyLink(user.getId(),
+					KeyLink.EMAIL_VALIDATION);
+			keyLinkDao.insertKeyLink(keylink);
+			NotificationManager notifier = NotificationManager.getInstance();
+			VerifyAccountObservable vao = notifier.getVerifyAccountObservable();
+
+			vao.userRegister(
+					user.getName() + " " + user.getSurname(),
+					user.getEmail(),
+					"http://localhost:8080/mdissphoto/p/validate/"
+							+ keylink.getId());
+		} else {
+			valid = false;
+		}
+		String outcome = "pretty:validateconfirmation";
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.getApplication().getNavigationHandler()
+				.handleNavigation(facesContext, null, outcome);
 	}
 
 	public String getKey() {
@@ -68,4 +109,13 @@ public class ValidateBean {
 						ParamsBean.class);
 		return pb;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
