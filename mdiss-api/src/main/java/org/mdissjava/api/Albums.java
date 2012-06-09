8eac85dd96bd194bd1cc1f26f36f4ae169545505
Xabier Larrakoetxea <slok69@gmail.com>
2012-06-09 12:33:17 +0200

    Added POST API method that creates an album

--- mdiss-api/src/main/java/org/mdissjava/api/Albums.java
+++ mdiss-api/src/main/java/org/mdissjava/api/Albums.java
@@ -1,9 +1,13 @@
 package org.mdissjava.api;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,11 +45,37 @@ public class Albums {
 		
 		AlbumDao albumDao = new AlbumDaoImpl(this.datastore);
 		List<Album> albums = albumDao.findAlbum(album);
-		
+		System.out.println("yeah");
 		if (albums.size() == 1)
 			return Response.status(200).entity(albums.get(0)).build();
 		else
 			return Response.status(400).entity("Error retrieving album").build();
+	}
+	
+	@POST
+	@Consumes("application/json")
+	public Response createAlbum(Album album){
+		
+		//Test: curl -i -X POST -H Accept:application/json -H Content-Type:application/json  -d '{"title":"Me","userNick":"horl"}' '127.0.0.1:8080/mdissapi/api/1.0/albums/'
+		if (album != null){
+			
+			AlbumDao albumDao = new AlbumDaoImpl(this.datastore);
+			
+			//search if the title exists
+			Album a = new Album();
+			a.setTitle(album.getTitle());
+			a.setUserNick(album.getUserNick());
+			if (albumDao.findAlbum(a).size() > 0)
+				return Response.status(200).entity("Album already exists").build();
+			
+			//save in database
+			album.setAlbumId(UUID.randomUUID().toString());
+			album.setCreationDate(new Date());
+			albumDao.insertAlbum(album);
+			return Response.status(400).entity(album).build();
+			
+		}else
+			return Response.status(200).entity("Error creating album").build();
 	}
 
 }
