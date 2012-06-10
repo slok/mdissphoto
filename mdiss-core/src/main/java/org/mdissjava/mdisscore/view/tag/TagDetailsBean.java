b19766be0c5c780a116db813fda85469e38c016a
David <David@10.164.19.75>
2012-06-11 00:26:15 +0200

    create Bean to show the details from the tag

--- mdiss-core/src/main/java/org/mdissjava/mdisscore/view/tag/TagDetailsBean.java
+++ mdiss-core/src/main/java/org/mdissjava/mdisscore/view/tag/TagDetailsBean.java
@@ -1,12 +1,9 @@
 package org.mdissjava.mdisscore.view.tag;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -26,15 +23,12 @@ public class TagDetailsBean {
 	
 	private final String GLOBAL_PROPS_KEY = "globals";
 	private final String MORPHIA_DATABASE_KEY = "morphia.db";
-	private final String RESOLUTIONS_PROPS_KEY = "resolutions";
 	private TagManagerImpl tagManager;
 	
 	private String userNick;
 	private String tag;	
 	private List<Photo> photos;
 	private Map<String,String> detailedPhotoURL;	
-	private List<String> defaultPhotoSizes;
-	private final int PHOTO_SHOW_SIZE = 640;
 	
 	public TagDetailsBean() {
 		ParamsBean pb = getPrettyfacesParams();
@@ -54,68 +48,14 @@ public class TagDetailsBean {
 			//get the photo list which contains that tag
 			this.photos = tagManager.getPhotosFromTag(this.tag);
 			
-			//search the available sizes for this photo
-			//int sizes[] = {100, 240, 320, 500, 640, 800, 1024}; //our different sizes
-			Properties allResolutions = propertiesFacade.getProperties(RESOLUTIONS_PROPS_KEY);
-			
-			this.defaultPhotoSizes = new ArrayList<String>();
-			
 			this.detailedPhotoURL = new HashMap<String, String>();
 			//get the PhotoURL from all the photos 
-			for (Photo p : this.photos) {				
-				//set the size
-				int height = p.getMetadata().getResolutionREAL().getHeight();
-				int width = p.getMetadata().getResolutionREAL().getWidth();
-				System.out.println("height: " + height);
-				System.out.println("height: " + height);
+			for (Photo p : this.photos) {												
 				
-				int photoSize = height > width ? height: width;
-				
-				// we get all the available resolutions
-				@SuppressWarnings("rawtypes")
-				Enumeration resolutions = allResolutions.keys();
-				
-				String key;
-				//for each one we check if is scalar one and not square and if is smaller than the photo
-				while(resolutions.hasMoreElements())
-				{
-					key = (String)resolutions.nextElement();
-					System.out.println("key: " + key);
-					
-					//Only needed the scale ones, not the squares
-					if (allResolutions.getProperty(key).contains("scale"))
-					{
-						//if is bigger than our photo size then don't add to the list of available sizes for this photo
-						if (photoSize >= Integer.valueOf(key))
-						{
-							this.defaultPhotoSizes.add(key);
-						}
-					}
-					
-				}
-				
-				
-				
-				// we want to know which is the best photo for the display of the detail. 
-				// Max is 640px but some photos are smaller than 640px so we set the original size
-				//and if the photo is bigger than 640 then set the size to 640
 				//get the database of the photos and create the url with the appropiate image
-				String bucket;
-				String bucketPropertyKey = null;
-				if(photoSize > PHOTO_SHOW_SIZE)//640px size
-				{
-					System.out.println("mayor");
-					bucketPropertyKey = "thumbnail.scale." + PHOTO_SHOW_SIZE + "px.bucket.name";
-					bucket = propertiesFacade.getProperties("thumbnails").getProperty(bucketPropertyKey);
-					bucket = "square.75";
-				}
-				else//original size
-				{
-					System.out.println("menor");
-					bucketPropertyKey = "images.bucket";
-//					bucket = propertiesFacade.getProperties("globals").getProperty(bucketPropertyKey);
-					bucket = "square.75";
-				}
+				String bucketPropertyKey = "images.bucket";
+				String bucket = propertiesFacade.getProperties("globals").getProperty(bucketPropertyKey);
+				bucket = "square.75";
 				
 				database = propertiesFacade.getProperties("globals").getProperty("images.db");
 				// get the photoURL which contains that tag				
@@ -166,14 +106,6 @@ public class TagDetailsBean {
 	public void setPhotosFromTag(List<Photo> photosFromTag) {
 		this.photos = photosFromTag;
 	}
-
-	public List<String> getDefaultPhotoSizes() {
-		return defaultPhotoSizes;
-	}
-	
-	public void setDefaultPhotoSizes(List<String> defaultPhotoSizes) {
-		this.defaultPhotoSizes = defaultPhotoSizes;
-	}	
 
 	public Map<String, String> getDetailedPhotoURL() {
 		return detailedPhotoURL;
