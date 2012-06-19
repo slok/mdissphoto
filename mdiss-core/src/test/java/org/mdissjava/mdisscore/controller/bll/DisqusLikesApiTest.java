cad6b219252a45eaf483b0bbaf3bd8ef573e8c59
Xabier Larrakoetxea <slok69@gmail.com>
2012-06-19 12:54:22 +0200

    Deactivate disqus tests

--- mdiss-core/src/test/java/org/mdissjava/mdisscore/controller/bll/DisqusLikesApiTest.java
+++ mdiss-core/src/test/java/org/mdissjava/mdisscore/controller/bll/DisqusLikesApiTest.java
@@ -19,7 +19,7 @@ public class DisqusLikesApiTest {
 	
 	final private String DISQUS_CONSUMER_KEY ="http://jboss.mdiss.info/mdissphoto/u/cerealguy/photo/7e9ff298-3e8c-4d8a-8ec5-5a0b83d57fea/";
 	
-	@Test
+	//@Test
 	public void callDisqusTest() throws IllegalArgumentException, IOException {
 		DisqusJsonReader dqr=new DisqusJsonReader();
 		int likes = 0,dislikes;
@@ -36,7 +36,7 @@ public class DisqusLikesApiTest {
 		assertTrue(likes==1);	
 	}
 	
-	@Test
+	//@Test
 	public void callAlternateDisqusTest() throws IllegalArgumentException, IOException {
 		DisqusJsonReader dqr=new DisqusJsonReader();
 		int likes;
